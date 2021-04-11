dbe80fd56a2f808b399f99692e04929e279ca5dd
Christof Schöch <c.schoech@gmail.com>
2021-04-11 17:21:11 +0200

    +code

--- code/Thema-1/thema-1_vollständig.py
+++ code/Thema-1/thema-1_vollständig.py
@@ -6,3 +6,11 @@ with open(textdatei, "r", encoding="utf8") as infile:
     print(text[0:100])
     zeichenzahl = len(text)
     print(zeichenzahl)
+    counter = 0
+    suchzeichen = "n"
+    for item in text:
+        if item == suchzeichen:
+            counter = counter + 1
+    print("Das Suchzeichen " + suchzeichen + "kommt im Text " + str(counter) + " Mal vor.")
+            
+        
