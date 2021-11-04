2a1d31c4fd48585d94ebea074ca7264863b02e4d
Christof Schöch <c.schoech@gmail.com>
2021-11-04 12:54:06 +0100

    thema-1 neu

--- code/Thema-1/thema-1_start.py
+++ code/Thema-1/thema-1_start.py
@@ -1,11 +1,8 @@
-
-
-from os.path import join
-
-textdatei = join("Kraus.txt")
+textdatei = "data/Kraus.txt"
 
 with open(textdatei, "r", encoding="utf8") as infile:
     text = infile.read()
-    print(text)
+    print(text[0:50])
+    print(text.lower()[0:50])
     print(type(text))
     print(len(text))
